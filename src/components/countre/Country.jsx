b1c77a2e8efc087112b14944b9df6c8885edd836
Idba1 <moniraislamself@gmail.com>
2024-03-14 12:00:38 +0600

    display all flags

--- src/components/countre/Country.jsx
+++ src/components/countre/Country.jsx
@@ -1,9 +1,10 @@
 import './country.css'
 const Country = ({country}) => {
-    const {name} = country
+    const {name, flags} = country
     return (
         <div className='body'>
             <h4>Name: {name?.common}</h4>
+            <img src={flags.png}/>
             {/* <p>cca3: {country.cca3}</p> */}
         </div>
     );
