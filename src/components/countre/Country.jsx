7c38abf2f3f2250a39dfeccbc2584bf9086e96af
Idba1 <moniraislamself@gmail.com>
2024-03-14 12:21:28 +0600

    display some data

--- src/components/countre/Country.jsx
+++ src/components/countre/Country.jsx
@@ -1,10 +1,15 @@
 import './country.css'
 const Country = ({country}) => {
-    const {name, flags} = country
+    const {name, flags, currencies,region} = country
     return (
         <div className='body'>
-            <h4>Name: {name?.common}</h4>
             <img src={flags.png}/>
+            <h4>Name: {name?.common}</h4>
+            <h4>Official Name: {name?.official}</h4>
+            <h5>Native Name: {name?.nativeName?.ell?.official}</h5>
+            <p>Currencies: {currencies?.EUR?.name}</p>
+            <p>symbol: {currencies?.EUR?.symbol}</p>
+            <h5>region: {region}</h5>
             {/* <p>cca3: {country.cca3}</p> */}
         </div>
     );
