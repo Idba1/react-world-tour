eb98facf7aab9388a529108b95b9240e3d2b2c99
Idba1 <moniraislamself@gmail.com>
2024-03-14 12:38:20 +0600

    handle click for check visit

--- src/components/countre/Country.jsx
+++ src/components/countre/Country.jsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
 import './country.css'
-const Country = ({country}) => {
-    const {name, flags, currencies,region} = country
+const Country = ({ country }) => {
+
+    const { name, flags, currencies, region } = country
+
+    const [Visited, setVisited] = useState(false)
+
+    const handleVisitedClick = () => {
+        setVisited(!Visited)
+    }
     return (
         <div className='body'>
-            <img src={flags.png}/>
+            <img src={flags.png} />
             <h4>Name: {name?.common}</h4>
             <h4>Official Name: {name?.official}</h4>
             <h5>Native Name: {name?.nativeName?.ell?.official}</h5>
@@ -11,6 +19,10 @@ const Country = ({country}) => {
             <p>symbol: {currencies?.EUR?.symbol}</p>
             <h5>region: {region}</h5>
             <p><small>cca3: {country.cca3}</small></p>
+            <button onClick={handleVisitedClick}>Going</button>
+            {
+                Visited? 'I have visited this country' : 'I want to visit'
+            }
         </div>
     );
 };
