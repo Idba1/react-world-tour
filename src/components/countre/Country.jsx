b788553e94a989e5afab17483ab2629aedf6e1a9
Idba1 <moniraislamself@gmail.com>
2024-03-15 12:07:27 +0600

    not complete

--- src/components/countre/Country.jsx
+++ src/components/countre/Country.jsx
@@ -1,27 +1,36 @@
 import { useState } from 'react';
 import './country.css'
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountries, handleClickFlage }) => {
 
-    const { name, flags, currencies, region } = country
+    const { name, flags, currencies, region, png } = country
 
+    // console.log(handleVisitedCountries)
     const [Visited, setVisited] = useState(false)
 
     const handleVisitedClick = () => {
         setVisited(!Visited)
     }
+
+
     return (
-        <div className={`body ${Visited && 'visited'}`}>
+        <div className={`body ${Visited ? 'visited' : 'nonVisited'}`}>
             <img src={flags.png} />
-            <h4>Name: {name?.common}</h4>
+            <h4 style={{ color: Visited ? 'Red' : 'lightblue' }}>Name: {name?.common}</h4>
             <h4>Official Name: {name?.official}</h4>
             <h5>Native Name: {name?.nativeName?.ell?.official}</h5>
             <p>Currencies: {currencies?.EUR?.name}</p>
             <p>symbol: {currencies?.EUR?.symbol}</p>
             <h5>region: {region}</h5>
             <p><small>cca3: {country.cca3}</small></p>
+            <button onClick={() => { handleVisitedCountries(country) }}>Mark As Visited</button>
+
+            <button onClick={() => { handleClickFlage(country.flags.png) }}>Mark As Visited</button>
+
+            <br />
+            <br />
             <button onClick={handleVisitedClick}>Going</button>
             {
-                Visited? 'I have visited this country' : 'I want to visit'
+                Visited ? 'I have visited this country' : 'I want to visit'
             }
         </div>
     );
--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -5,17 +5,54 @@ import './countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([])
+    const [visitedCountries, setVisitedCountries] = useState([])
+    const [clickFlage, setClickFlage] = useState([])
+
+
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
             .then(res => res.json())
             .then(data => setCountries(data))
     }, [])
+
+    const handleVisitedCountries = country => {
+        console.log(country);
+        // console.log('mark as visited')
+        const newVisitedCountries = [...visitedCountries, countries]
+        setVisitedCountries(newVisitedCountries)
+    }
+
+    const handleClickFlage = flage => {
+        console.log(flage)
+        const newClickFlage = [...clickFlage, flage]
+        setClickFlage(newClickFlage);
+    }
+
     return (
         <div>
             <h2>All Countries : {countries.length}</h2>
+            <div>
+                <h5>Visited Countries: {visitedCountries.length}</h5>
+                {
+                    visitedCountries.map(country => <li key={country.cca3}>{country.capital}</li>)
+                }
+            </div>
+            <div>
+                <h5>Click Flag</h5>
+                {
+                    clickFlage.map(country => <li key={country.cca3}>{country.flags.png}</li>)
+                }
+            </div>
             <div className="countries-container">
                 {
-                    countries.map(country => <Country country={country} key={country.cca3}></Country>)
+                    countries.map(country =>
+                        <Country
+                            country={country}
+                            handleVisitedCountries={handleVisitedCountries}
+                            handleClickFlage ={handleClickFlage}
+                            key={country.cca3}
+                        >
+                        </Country>)
                 }
             </div>
         </div>
